6fed790e90e456d80acacbe11182e445afd7452f
Yuri Cicconi <yuricbianchii@gmail.com>
2023-04-26 11:06:58 -0300

    solução para o desafio proposto pela Alura

--- src/Compra.java
+++ src/Compra.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Compra {
+public class Compra implements Comparable<Compra>{
+
+    private String produto;
+    private double valor;
+
+    public Compra(String produto, double valor) {
+        this.produto = produto;
+        this.valor = valor;
+    }
+
+    public String getProduto() {
+        return produto;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra: \n" +
+                "produto: '" + produto + '\'' +
+                "-- valor: " + valor + "\n";
+    }
+
+    @Override
+    public int compareTo(Compra outraCompra) {
+        return Double.valueOf(this.getValor()).compareTo(Double.valueOf(outraCompra.getValor()));
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int continua = 1;
+
+        System.out.println("Informe o limite do cartão: ");
+        double limite = sc.nextDouble();
+
+        CartaoDeCredito cartao = new CartaoDeCredito(limite);
+
+
+
+        while (continua == 1) {
+            System.out.println("Informe o produto que deseja comprar: ");
+            String produto = sc.next();
+
+            System.out.println("Informe o valor do produto: ");
+            double valor = sc.nextDouble();
+
+            sc.nextLine();
+
+            Compra compra = new Compra(produto, valor);
+            boolean compraAprovada = cartao.validaCompra(compra);
+
+            if (compraAprovada) {
+                System.out.println("Compra aprovada!");
+                System.out.println("Digite 1 para continuar comprando ou 0 para sair");
+                continua = sc.nextInt();
+            } else {
+                System.out.println("Saldo insuficiente");
+                System.out.println("Digite 1 para continuar comprando ou 0 para sair");
+                continua = sc.nextInt();
+            }
+        }
+
+        Collections.sort(cartao.getCompras());
+
+        System.out.println("*********************************************");
+        System.out.println("Compras\n");
+
+        for (Compra compra : cartao.getCompras()) {
+            System.out.println("Item - " + compra.getProduto() + " valor - " + compra.getValor());
+        }
+
+        System.out.println(cartao.getCompras());
     }
 }
